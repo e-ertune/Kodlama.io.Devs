5c7227a148fcf68785c0bb7639427f81f6776570
e-ertune <emin.ertune@hotmail.com>
2022-09-06 11:30:21 +0300

    Initial commit

---
+++ src/projects/case/Application/Features/ProgrammingLanguages/Commands/CreateProgrammingLanguage/CreateProgrammingLanguageCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Application.Features.ProgrammingLanguages.Commands.CreateProgrammingLanguage
+{
+    public class CreateProgrammingLanguageCommandValidator : AbstractValidator<CreateProgrammingLanguageCommand>
+    {
+        public CreateProgrammingLanguageCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty();
+        }
+    }
+}
---
+++ src/projects/case/Domain/Entities/ProgrammingLanguage.cs
@@ -0,0 +1,20 @@
+﻿using Core.Persistence.Repositories;
+
+namespace Domain.Entities
+{
+    public class ProgrammingLanguage : Entity
+    {
+        public string Name { get; set; } = null!;
+
+        public ProgrammingLanguage(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public ProgrammingLanguage()
+        {
+
+        }
+    }
+}
